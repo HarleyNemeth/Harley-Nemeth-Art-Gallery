7b192e070e696763a2f91bb632809ab487c798d8
Harley Nemeth <harleynemeth17@gmail.com>
2026-06-18 21:50:33 +0100

    allowing railway url host

--- nemeth_art/settings.py
+++ nemeth_art/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
+ALLOWED_HOSTS=web-production-1af7ef.up.railway.app,127.0.0.1,localhost
 
 # Application definition
 
