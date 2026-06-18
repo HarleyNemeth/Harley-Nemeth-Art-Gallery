0e8b4293a06c99e1e739eab2419511ee0d0aeb20
Harley Nemeth <harleynemeth17@gmail.com>
2026-06-18 21:56:09 +0100

    fixed allowed host url error

--- nemeth_art/settings.py
+++ nemeth_art/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS=web-production-1af7ef.up.railway.app,127.0.0.1,localhost
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
 
